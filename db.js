bea9211255438a48903cd0ddf3dfc810942506da
PA1YY <yoppianugrah00@gmail.com>
2026-05-29 11:10:34 +0700

    config/db.js

---
+++ db.js
@@ -0,0 +1,12 @@
+// In-Memory Database
+const users = [];
+const transactions = [];
+let userIdCounter = 1;
+let transactionIdCounter = 1;
+
+module.exports = {
+    users,
+    transactions,
+    userIdCounter,
+    transactionIdCounter
+};
